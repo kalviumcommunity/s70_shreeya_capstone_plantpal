41aaa84409e6fdf7f3855b7642b73ffb47facc24
Alexa <shreeya.bv.s70@kalvium.community>
2025-04-15 17:04:20 +0530

    committed api backend

--- models/Plant.js
+++ models/Plant.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose')
 
+
+
 const plantSchema = new mongoose.Schema({
     plantName : String,
     plantSpecies : String,
     description : String,
     addedOn : {type : Date, default : Date.now()}
 });
+
+
+
 
 const Plant = mongoose.model("Plant", plantSchema)
 module.exports = Plant
